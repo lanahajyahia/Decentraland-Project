035d91a98c405412f4f960fa1966df7f550fa7f9
lanahajyahia <46194686+lanahajyahia@users.noreply.github.com>
2022-07-16 16:10:29 +0300

    changes

--- client/src/components/EditLand/EditLand.js
+++ client/src/components/EditLand/EditLand.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import "./EditLand.css";
+import { Form, Row, FloatingLabel } from "react-bootstrap";
 
 const EditLand = (props) => {
+  const [editStatus, setEditStatus] = useState(props.popupItem.name);
+  const [editPrice, setEditPrice] = useState(props.popupItem.price);
+
+  console.log("edit stas", editStatus);
+  console.log("editPrice", props.popupItem.price);
+
   return props.trigger ? (
     <div className="editLand">
-      EditLand
       <div className="inner-editLand">
-        inner EditLand
-        <button className="cancel-edit-btn">cancel</button>
-        <button className="save-land-btn">save</button>
+        {/* status select  */}
+        <Row>Land's status</Row>
+        <Form.Select
+          aria-label="land status select"
+          onChange={(e) => setEditStatus(e.currentTarget.value)}
+          defaultValue={props.popupItem.name}
+        >
+          <option value="forSale">For Sale</option>
+          <option value="notForSale">Not For Sale</option>
+        </Form.Select>
+        <br></br>
+        <Row>Land's price</Row>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Price between 15$-200$"
+          className="mb-3"
+          onChange={(e) => setEditPrice(e.target.value)}
+        >
+          <Form.Control
+            defaultValue={props.popupItem.price}
+            type="number"
+            placeholder="price$"
+          />
+        </FloatingLabel>
+        <button
+          className="cancel-edit-btn"
+          onClick={() => {
+            props.setEditTrigger(false);
+            props.setLandPopup(true);
+          }}
+        >
+          Cancel
+        </button>
+        <button className="save-land-btn">Save</button>
       </div>
     </div>
   ) : (
--- client/src/components/Popup/Popup.js
+++ client/src/components/Popup/Popup.js
@@ -1,61 +1,83 @@
 import React, { useState } from "react";
 import EditLand from "../EditLand/EditLand";
+import { Row } from "react-bootstrap";
+
 import "./Popup.css";
 const Popup = (props) => {
   const loggedUser = JSON.parse(localStorage.getItem("userInfo"));
 
   const [editTrigger, setEditTrigger] = useState(false);
-  console.log(
-    "loggedUser",
-    loggedUser.lands,
-    props.popupItem._id,
-    loggedUser.lands.indexOf(props.popupItem._id),
-    loggedUser.lands.map((object) => object._id).indexOf(props.popupItem._id)
-  );
+  const [landPopUp, setLandPopup] = useState(true);
+
+  // console.log(
+  //   "loggedUser",
+  //   loggedUser.lands,
+  //   props.popupItem._id,
+  //   loggedUser.lands.indexOf(props.popupItem._id),
+  //   loggedUser.lands.map((object) => object._id).indexOf(props.popupItem._id)
+  // );
   //trigger is a true or false val to trigger the popup
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <div id="land-popup">
-          <button
-            className="close-btn"
-            onClick={() => props.setClosePopupTrigger(false)}
-          >
-            X
-          </button>
-          <div id="Square name">{props.popupItem.name}</div>
-          {(props.popupItem.name === "forSale" ||
-            props.popupItem.name === "notForSale") && (
-            <div>Price: {props.popupItem.price}$</div>
-          )}
-        </div>
-        {
-          /* allow edit only if user's Land */
-          // same check think to remove one !
-          loggedUser._id === props.popupItem.owner &&
-            loggedUser.lands
-              .map((object) => object._id)
-              .indexOf(props.popupItem._id) > -1 && (
-              <button className="edit-btn" onClick={() => setEditTrigger(true)}>
-                edit
-              </button>
-            )
-        }
-        <EditLand trigger={editTrigger}></EditLand>
-        {
-          /* allow edit only if user's Land */
-          loggedUser._id !== props.popupItem.owner &&
-            props.popupItem.name === "forSale" &&
-            loggedUser.isBuyer && (
-              <button
-                className="buy-land-btn"
-                // on click check user budget if he can buy
-                // onClick={() => props.setClosePopupTrigger(false)
-              >
-                Buy Land
-              </button>
-            )
-        }
+        <button
+          className="close-btn"
+          onClick={() => {
+            props.setClosePopupTrigger(false);
+            setEditTrigger(false);
+            setLandPopup(true);
+          }}
+        >
+          X
+        </button>
+        {landPopUp && (
+          <div id="land-popup">
+            <Row id="SquareName">{props.popupItem.name}</Row>
+            {(props.popupItem.name === "forSale" ||
+              props.popupItem.name === "notForSale") && (
+              <Row id="squarePrice">Price: {props.popupItem.price}$</Row>
+            )}
+            {
+              /* allow edit only if user's Land */
+              // same check think to remove one !
+              loggedUser._id === props.popupItem.owner &&
+                loggedUser.lands
+                  .map((object) => object._id)
+                  .indexOf(props.popupItem._id) > -1 && (
+                  <button
+                    className="edit-btn"
+                    onClick={() => {
+                      setEditTrigger(true);
+                      setLandPopup(false);
+                    }}
+                  >
+                    edit
+                  </button>
+                )
+            }
+            {
+              /* allow edit only if user's Land */
+              loggedUser._id !== props.popupItem.owner &&
+                props.popupItem.name === "forSale" &&
+                loggedUser.isBuyer && (
+                  <button
+                    className="buy-land-btn"
+                    // on click check user budget if he can buy
+                    // onClick={() => props.setClosePopupTrigger(false)
+                  >
+                    Buy Land
+                  </button>
+                )
+            }
+          </div>
+        )}
+        <EditLand
+          trigger={editTrigger}
+          setEditTrigger={setEditTrigger}
+          setLandPopup={setLandPopup}
+          popupItem={props.popupItem}
+        ></EditLand>
+
         {props.children}
       </div>
     </div>
